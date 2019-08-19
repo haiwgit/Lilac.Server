7bd756d962d6551cd6680ea7efd9ec115e0e3873
haiwgit <525518487@qq.com>
2019-08-19 19:10:39 +0800

    update

---
+++ Infrastructure/LilacCore/IdGenerator/NullGenerateGuidIDGenerator.cs
@@ -0,0 +1,36 @@
+﻿using NHibernate;
+using NHibernate.Id;
+using NHibernate.Type;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lilac.LilacCore.IdGenerator
+{
+    public class NullGenerateGuidIDGenerator : IIdentifierGenerator, IConfigurable
+    {
+        string entityName;
+
+        public object Generate(NHibernate.Engine.ISessionImplementor session, object obj)
+        {
+
+            object id = session.GetEntityPersister(entityName, obj).GetIdentifier(obj, session.EntityMode);
+            if (id == null)
+            {
+                id = Guid.NewGuid().ToString();
+            }
+            return id;
+        }
+
+        public void Configure(IType type, IDictionary<string, string> parms, NHibernate.Dialect.Dialect dialect)
+        {
+            parms.TryGetValue(IdGeneratorParmsNames.EntityName, out entityName);
+            if (entityName == null)
+            {
+                throw new MappingException("no entity name");
+            }
+        }
+    }
+}
---
+++ Modules/SystemManager/Mappings/UserMap.cs
@@ -0,0 +1,23 @@
+﻿using FluentNHibernate.Mapping;
+using Lilac.SystemManager.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lilac.SystemManager.Mappings
+{
+    public class UserMap:ClassMap<User>
+    {
+        public UserMap()
+        {
+            Not.LazyLoad();
+            Table("ss_users");
+            Id(p => p.ID, "ID");
+            Map(p => p.Account, "ACCOUNT");
+            Map(p => p.Name, "NAME");
+            Map(p => p.Password, "PASSWORD");
+        }
+    }
+}
